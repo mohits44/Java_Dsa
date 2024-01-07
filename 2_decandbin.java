6b4f1e7cbf53d077480769fbd15e7e6e06f23bbc
mohits44 <101943239+mohits44@users.noreply.github.com>
2024-01-07 12:54:37 +0530

    Update 2_decandbin.java

--- 2_decandbin.java
+++ 2_decandbin.java
@@ -1,1 +1,20 @@
-
+class Decandbin {
+    public static void main(String[] args) {
+         HelloWorld obj = new HelloWorld();
+         System.out.println(obj.dTob(10));
+    }
+    int dTob(int n)
+    {
+        int rem;
+        int i=0;
+        int bin=0;
+        while(n!=0)
+        {
+            rem=n%2;
+            bin=rem*(int)Math.pow(10,i)+bin;
+            n=n/2;
+            i++;
+        }
+        return bin;
+    }
+}
